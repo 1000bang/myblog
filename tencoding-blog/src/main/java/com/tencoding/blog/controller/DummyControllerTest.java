3ecc420fcbd85184f471c42ebd42b7ef88476ae5
1000bang <1000_bang@naver.com>
2022-12-09 11:21:16 +0900

    12/9 #11 update 및 더티체킹 사용

--- tencoding-blog/src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
+++ tencoding-blog/src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
@@ -1,12 +1,21 @@
 package com.tencoding.blog.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tencoding.blog.dto.User;
@@ -16,12 +25,13 @@ import com.tencoding.blog.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
+@RequestMapping("/dummy")
 public class DummyControllerTest {
 
 	@Autowired //순환참조 조심
 	private UserRepository userRepository;
 	
-	@GetMapping("/dummy/user/{id}")
+	@GetMapping("/user/{id}")
 	public Optional<User>  detail(@PathVariable int id) {
 		Optional<User> user = userRepository.findById(id);
 		return user;
@@ -42,7 +52,7 @@ public class DummyControllerTest {
 //	}
 	
 	// http://localhost:9090/blog/dummy/signup
-	@PostMapping("/dummy/signup")
+	@PostMapping("/signup")
 	public String SignUp(@RequestBody User user) {
 
 		log.info(">>>User : {} ", user);
@@ -52,6 +62,45 @@ public class DummyControllerTest {
 		return "회원가입이 완료되었습니다";
 	}
 	
+	// select all 전체보기
+    @GetMapping("/users")
+    public List<User> list(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users2")
+    public List<User> pageList(@PageableDefault(size = 2, 
+            sort = "id", direction = Direction.DESC) Pageable pageable){
+        System.out.println("11111111111111");
+    	Page<User> userPage = userRepository.findAll(pageable);
+		
+        
+        List<User> users = userRepository.findAll(pageable).getContent();
+        return users;
+    }
 	
+    //회원수정
+    //@Transactional //트랜잭션 기본의미, 함수종료시 더티체킹하고 수정된 데이터가 있다면 commit이 된다.    
+    @PutMapping("/user/{id}")
+	public User udateUser(@PathVariable int id, @RequestBody User reqUser) {
+
+		log.info(">>> id : {}, >>> password >>> {}, email >>> {}", 
+				id, reqUser.getPassword(), reqUser.getEmail());
+		
+		// 사용자 여부를 먼저 확인 (select) 
+		// 사용자 있다면 넘겨 받은 데이터를 가공해서 -> 저장 
+		// 사용자가 없다면 클라언트에게 잘못된 요청, 없는 사용자 입니다. 
+		
+		User user =  userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정에 실패 하였습니다.");
+		});
+		
+		user.setPassword(reqUser.getPassword());
+		user.setEmail(reqUser.getEmail());
+		
+		userRepository.save(user);
+		
+		return null; 
+	}
 	
 }
