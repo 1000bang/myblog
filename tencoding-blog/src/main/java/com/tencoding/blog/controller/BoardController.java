ab9a1d8c03f0cff243f80d212142698e4a9ca3f3
1000bang <1000_bang@naver.com>
2022-12-19 12:12:39 +0900

    12/19 #35 페이징처리

--- tencoding-blog/src/main/java/com/tencoding/blog/controller/BoardController.java
+++ tencoding-blog/src/main/java/com/tencoding/blog/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.tencoding.blog.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,13 +32,35 @@ public class BoardController {
 		Page<Board> boards = boardService.getBoardList(pageable);
 		
 		
+		// 1. 현재 페이지 앞뒤로 2칸씩 보이기
+		// 2. 현재페이지 active 처리하기
+		// 3. 페이지 숫자를 눌렀을 경우 해당 페이지로 화면 이동하기 
 		
-		//page.first = true, false <- 첫번째 페이지면  true
-		//page.last = true, false <- true 마지막 페이지 
-						
+		// 총 몇페이지가 나오는지 확인 
+		System.out.println(">>>>>>화면에 보여줄 게시글 수  : " + boards.getSize());
+		System.out.println(">>>>>>전체 페이지 크기 : " + boards.getTotalPages());
+		System.out.println(">>>>>>현재 페이지 번호 : " + boards.getPageable().getPageNumber());
+		
+		int nowPage = boards.getPageable().getPageNumber() + 1;
+		
+		int startPageNumber = Math.max(nowPage -2, 1);
+		int endPageNumber = Math.min(nowPage + 2, boards.getTotalPages());
+	
+		System.out.println(">>>>>>시작해야하는 번호 : " + startPageNumber);
+		System.out.println(">>>>>>마지막  번호 : " + endPageNumber);
+
+		ArrayList<Integer> pageNumbers = new ArrayList<>();
+		for (int i = startPageNumber; i < endPageNumber + 1; i++) {
+			pageNumbers.add(i);
+		}
 		
 		model.addAttribute("boards", boards);  // -> jsp파일에서 model 추상화객체를 이용해 컨트롤러에서 내려준 데이터 접근할 수 있다  
+		model.addAttribute("nowPage", nowPage);
+		model.addAttribute("startPage", startPageNumber);
+		model.addAttribute("endPage", endPageNumber);
+		model.addAttribute("pageNumbers", pageNumbers);
 		
+	
 		
 		return "index";
 	}
--- tencoding-blog/src/main/webapp/WEB-INF/views/index.jsp
+++ tencoding-blog/src/main/webapp/WEB-INF/views/index.jsp
@@ -19,6 +19,18 @@
   <c:set var = "isNotDisabled" value = ""> </c:set>
 	<li class="page-item ${boards.first ? isDisabled : isNotDisabled}"><a class="page-link" href="?page=${boards.number - 1}" >Previous</a></li>
 	
+	<c:forEach var="num" items="${pageNumbers}">
+		<c:choose>
+		<c:when test="${nowPage eq num}">
+		<li class="page-item active"><a class="page-link" href="?page=${num - 1}">${num}</a></li>
+		</c:when>
+		<c:otherwise>
+		<li class="page-item"><a class="page-link" href="?page=${num - 1}">${num}</a></li>
+		</c:otherwise>
+		</c:choose>
+ 	 </c:forEach>
+	
+	
 	<li class="page-item ${boards.last ? isDisabled : isNotDisabled}"><a class="page-link" href="?page=${boards.number + 1}">Next</a></li>
 </ul>
 
